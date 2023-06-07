d53eb2969a90681264647fcb7e0850de9083acb5
Sofenos <soufianainos@gmail.com>
2023-06-07 23:58:03 +0000

    Optional

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def print_tebahpla():
+    for i in range(ord('z'), ord('a') - 1, -1):
+        print(chr(i) if i % 2 == 1 else chr(i).upper(), end='')
+
+print_tebahpla()
+print("")
