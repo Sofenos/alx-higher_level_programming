7f9573276c71575a1d4779fbdef09b7cc0fc1413
Sofenos <soufianainos@gmail.com>
2023-06-07 01:54:27 +0000

    luuuwyah

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,12 +2,8 @@
 
 def pow(a, b):
     result = 1
-    for _ in range(b):
+    for _ in range(abs(b)):
         result *= a
+    if b < 0:
+        result = 1 / result
     return result
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
