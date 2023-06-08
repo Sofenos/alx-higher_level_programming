65f5e6b0c34c01b85cd2c76827d3ac201b1d9392
Sofenos <soufianainos@gmail.com>
2023-06-08 00:09:17 +0000

    Luyah

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_tebahpla():
     for i in range(ord('z'), ord('A') - 1, -1):
-        print("{}".format(chr(i) if i % 2 == 0 else chr(i).lower()), end='')
+        if i % 2 == 0:
+            print('{}'.format(chr(i)), end='')
+        else:
+            print('{}'.format(chr(i).lower()), end='')
 print_tebahpla()
 print("")
